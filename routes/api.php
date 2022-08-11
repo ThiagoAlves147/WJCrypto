da770ec554bfc5d7156991ef2c45259d72250d1c
Thiago Alves <thiago1.4.7@outlook.com>
2022-08-11 09:13:39 -0300

    Adicionando autenticação

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,17 @@ use App\Controllers\Api\StatementController;
 use App\Controllers\Api\TestController;
 use App\Controllers\Api\TransactionController;
 use App\Controllers\Api\UserController;
+use App\Middlewares\Auth;
 use App\Router;
 
 Router::group(['prefix' => '/wjcrypto/api/'], function(){
-    Router::get('ping', [TestController::class, 'test']);
-    Router::post('users/create', [UserController::class, 'createUser']);
-    Router::post('users/deposit', [TransactionController::class, 'depositValue']);
-    Router::post('users/transfer', [TransactionController::class, 'transferValue']);
-    Router::post('users/withdrawn', [TransactionController::class, 'withdrawnValue']);
     
-    Router::get('users/statements', [StatementController::class, 'getStatements']);
+    Router::group(['middleware', Auth::class], function(){
+        Router::post('users/create', [UserController::class, 'createUser']);
+        Router::post('users/deposit', [TransactionController::class, 'depositValue']);
+        Router::post('users/transfer', [TransactionController::class, 'transferValue']);
+        Router::post('users/withdrawn', [TransactionController::class, 'withdrawnValue']);
+        Router::get('users/statements', [StatementController::class, 'getStatements']);
+    });
+
 });
